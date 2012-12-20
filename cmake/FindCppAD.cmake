b175bdec08f99c645d3448dafdb922f4f0f9b7e1
joao <joao@fox2>
2012-12-20 11:01:53 +0000

    updated license in file headers

--- cmake/FindCppAD.cmake
+++ cmake/FindCppAD.cmake
@@ -1,3 +1,16 @@
+# ----------------------------------------------------------------------------
+# CppADCodeGen: C++ Algorithmic Differentiation with Source Code Generation:
+#   Copyright (C) 2012 Ciengis
+#
+# CppADCodeGen is distributed under multiple licenses:
+#
+#  - Common Public License Version 1.0 (CPL1), and
+#  - GNU General Public License Version 2 (GPL2).
+#
+# CPL1 terms and conditions can be found in the file epl-v10.txt, while
+# terms and conditions for the GPL2 can be found in the file gpl2.txt.
+# ----------------------------------------------------------------------------
+#
 # - Try to find CppAD
 # Once done this will define
 #  CPPAD_FOUND - System has CppAD
--- test/test_code_gen.cpp
+++ test/test_code_gen.cpp
@@ -1,13 +1,17 @@
 /* --------------------------------------------------------------------------
-CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-11 Bradley M. Bell
-
-CppAD is distributed under multiple licenses. This distribution is under
-the terms of the 
-                    Common Public License Version 1.0.
-
-A copy of this license is included in the COPYING file of this distribution.
-Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
--------------------------------------------------------------------------- */
+ *  CppADCodeGen: C++ Algorithmic Differentiation with Source Code Generation:
+ *    Copyright (C) 2012 Ciengis
+ *
+ *  CppADCodeGen is distributed under multiple licenses:
+ *
+ *   - Common Public License Version 1.0 (CPL1), and
+ *   - GNU General Public License Version 2 (GPL2).
+ *
+ * CPL1 terms and conditions can be found in the file "epl-v10.txt", while
+ * terms and conditions for the GPL2 can be found in the file "gpl2.txt".
+ * ----------------------------------------------------------------------------
+ * Author: Joao Leal
+ */
 
 // system include files used for I/O
 #include <iostream>
