6f60b6a0a79492841800a9f9c67f4b0676867377
joao <joao@floatingfox>
2013-10-11 15:19:50 +0100

    changed licenses to EPL1 and GPL3

--- cmake/FindDL.cmake
+++ cmake/FindDL.cmake
@@ -1,14 +1,14 @@
 # ----------------------------------------------------------------------------
-# CppADCodeGen: C++ Algorithmic Differentiation with Source Code Generation:
-#   Copyright (C) 2012 Ciengis
+#  CppADCodeGen: C++ Algorithmic Differentiation with Source Code Generation:
+#    Copyright (C) 2012 Ciengis
 #
-# CppADCodeGen is distributed under multiple licenses:
+#  CppADCodeGen is distributed under multiple licenses:
 #
-#  - Common Public License Version 1.0 (CPL1), and
-#  - GNU General Public License Version 2 (GPL2).
+#   - Eclipse Public License Version 1.0 (EPL1), and
+#   - GNU General Public License Version 3 (GPL3).
 #
-# CPL1 terms and conditions can be found in the file epl-v10.txt, while
-# terms and conditions for the GPL2 can be found in the file gpl2.txt.
+#  EPL1 terms and conditions can be found in the file "epl-v10.txt", while
+#  terms and conditions for the GPL3 can be found in the file "gpl3.txt".
 # ----------------------------------------------------------------------------
 #
 # - Try to find libdl
