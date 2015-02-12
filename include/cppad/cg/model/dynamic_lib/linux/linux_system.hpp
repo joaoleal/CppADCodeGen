1d3ce2caca8eedd171db21cf099684256e558f65
joao <joaoruileal@gmail.com>
2015-02-12 18:00:36 +0000

    improved system::callExecutable() for linux

--- include/cppad/cg/model/dynamic_lib/linux/linux_system.hpp
+++ include/cppad/cg/model/dynamic_lib/linux/linux_system.hpp
@@ -29,6 +29,58 @@ namespace cg {
  */
 namespace system {
 
+namespace {
+
+/**
+ * Utility class used to close automatically file descriptors
+ */
+class FDHandler {
+public:
+    int fd;
+    bool closed;
+public:
+
+    inline FDHandler() : closed(true) {
+    }
+
+    inline explicit FDHandler(int fd) : fd(fd), closed(false) {
+    }
+
+    inline void close() {
+        if (!closed) {
+            ::close(fd);
+            closed = true;
+        }
+    }
+
+    inline ~FDHandler() {
+        close();
+    }
+};
+
+/**
+ * Utility class for pipes
+ */
+class PipeHandler {
+public:
+    FDHandler read;
+    FDHandler write;
+public:
+
+    inline void create() throw (CGException) {
+        int fd[2]; /** file descriptors used to communicate between processes*/
+        if (pipe(fd) < 0) {
+            throw CGException("Failed to create pipe");
+        }
+        read.fd = fd[0];
+        read.closed = false;
+        write.fd = fd[1];
+        write.closed = false;
+    }
+};
+
+}
+
 template<class T>
 const std::string SystemInfo<T>::DYNAMIC_LIB_EXTENSION = ".so";
 
@@ -81,31 +133,36 @@ inline std::string filenameFromPath(const std::string& path) {
 
 inline void callExecutable(const std::string& executable,
                            const std::vector<std::string>& args,
-                           bool createPipe,
+                           bool pipeSource,
                            const std::string& pipeMessage) throw (CGException) {
-
     std::string execName = filenameFromPath(executable);
 
-    int fd[2];
+    PipeHandler pipeMsg; // file descriptors used to communicate between processes
+    pipeMsg.create();
 
-    if (createPipe) {
+    PipeHandler pipeSrc;
+    if (pipeSource) {
         //Create pipe for piping source to the compiler
-        CPPADCG_ASSERT_KNOWN(pipe(fd) == 0, "Failed to create pipe");
+        pipeSrc.create();
     }
 
     //Fork the compiler, pipe source to it, wait for the compiler to exit
     pid_t pid = fork();
-    CPPADCG_ASSERT_KNOWN(pid >= 0, "Failed to fork program");
+    if (pid < 0) {
+        throw CGException("Failed to fork process");
+    }
 
     if (pid == 0) {
-        //  Child process
+        /***********************************************************************
+         * Child process
+         **********************************************************************/
+        pipeMsg.read.close();
 
-        if (createPipe) {
-            // close write end of pipe
-            close(fd[1]);
+        if (pipeSource) {
+            pipeSrc.write.close(); // close write end of pipe
             // Send pipe input to stdin
             close(STDIN_FILENO);
-            dup2(fd[0], STDIN_FILENO);
+            dup2(pipeSrc.read.fd, STDIN_FILENO);
         }
 
         auto toCharArray = [](const std::string & args) {
@@ -125,48 +182,88 @@ inline void callExecutable(const std::string& executable,
         }
         args2.back() = (char *) nullptr; // END             
 
-        execv(executable.c_str(), &args2[0]);
+        int eCode = execv(executable.c_str(), &args2[0]);
 
         for (size_t i = 0; i < args.size(); i++) {
             delete [] args2[i];
         }
 
-        exit(0);
+        if (eCode < 0) {
+            char buf[512];
+            std::string error = executable + ": " + strerror_r(errno, buf, 511); // thread safe
+            ssize_t size = error.size() + 1;
+            if (write(pipeMsg.write.fd, error.c_str(), size) != size) {
+                std::cerr << "Failed to send message to parent process" << std::endl;
+            }
+            std::cerr << "*** ERROR: exec failed" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        exit(EXIT_SUCCESS);
     }
 
-    // Parent process
-    if (createPipe) {
+    /***************************************************************************
+     * Parent process
+     **************************************************************************/
+    pipeMsg.write.close();
+
+    auto readCErrorMsg = []() {
+        int error = errno;
+        errno = 0;
+        char buf[512];
+        return std::string(strerror_r(error, buf, 512));
+    };
+
+    std::string writeError;
+    if (pipeSource) {
         // close read end of pipe
-        close(fd[0]);
+        pipeSrc.read.close();
         //Pipe source to the executable
-        ssize_t flag = write(fd[1], pipeMessage.c_str(), pipeMessage.size());
-        close(fd[1]);
-        if (flag == -1)
-            throw CGException("Failed to write to pipe");
+        ssize_t writeFlag = write(pipeSrc.write.fd, pipeMessage.c_str(), pipeMessage.size());
+        if (writeFlag == -1)
+            writeError = readCErrorMsg() + " ";
+        pipeSrc.write.close();
     }
 
     //Wait for the executable to exit
     int status;
+    // Read message from the child
+    std::string message;
+    char buffer[128];
     do {
+        while (read(pipeMsg.read.fd, buffer, sizeof (buffer)) > 0) {
+            message += buffer;
+            if (message.size() > 1e4) break;
+        }
+
         if (waitpid(pid, &status, 0) < 0) {
-            int error = errno;
-            char* buf = new char[512];
             std::stringstream s;
-            s << "Waitpid failed for pid " << pid << " [" << strerror_r(error, buf, 512) << "]";
-            delete [] buf;
+            s << "Waitpid failed for pid " << pid << " [" << readCErrorMsg() << "]";
             throw CGException(s.str());
         }
     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+
+    pipeMsg.read.close();
+
+    if (!writeError.empty()) {
+        std::stringstream s;
+        s << "Failed to write to pipe";
+        if (!message.empty()) s << ": " << message;
+        else s << ": " << writeError;
+        throw CGException(s.str());
+    }
 
     if (WIFEXITED(status)) {
         if (WEXITSTATUS(status) != EXIT_SUCCESS) {
             std::stringstream s;
             s << "Executable '" << executable << "' (pid " << pid << ") exited with code " << WEXITSTATUS(status);
+            if (!message.empty()) s << ": " << message;
             throw CGException(s.str());
         }
     } else if (WIFSIGNALED(status)) {
         std::stringstream s;
         s << "Executable '" << executable << "' (pid " << pid << ") terminated by signal " << WTERMSIG(status);
+        if (!message.empty()) s << ": " << message;
         throw CGException(s.str());
     }
 }
