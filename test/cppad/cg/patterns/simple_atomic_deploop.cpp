4df1765bc6e9e15b8d1ad29506b5d75b1aacd1e9
joao <joaoruileal@gmail.com>
2015-09-23 11:20:56 +0100

    changed outer model of simple_atomic_deploop test

--- test/cppad/cg/patterns/simple_atomic_deploop.cpp
+++ test/cppad/cg/patterns/simple_atomic_deploop.cpp
@@ -50,8 +50,8 @@ protected:
 std::vector<ADCGD> outerModel(const std::vector<ADCGD>& x,
                               size_t repeat,
                               atomic_base<CGD>& atomic) {
-    size_t m = 4;
-    size_t n = 4;
+    size_t m = 8;
+    size_t n = 8;
     size_t m2 = repeat * m;
 
     // dependent variable vector 
@@ -67,6 +67,16 @@ std::vector<ADCGD> outerModel(const std::vector<ADCGD>& x,
         y[i * m + 1] = ay[1];
         y[i * m + 2] = ay[2];
         y[i * m + 3] = ay[3];
+
+        ax[0] = x[i * n + 4];
+        ax[1] = x[i * n + 5 ];
+        ax[2] = x[i * n + 6 ];
+        ax[3] = x[i * n + 7 ];
+        atomic(ax, ay);
+        y[i * m + 4] = ay[0];
+        y[i * m + 5] = ay[1];
+        y[i * m + 6] = ay[2];
+        y[i * m + 7] = ay[3];
     }
 
     return y;
@@ -86,13 +96,12 @@ void atomicFunction(const std::vector<AD<double> >& x, std::vector<AD<double> >&
 TEST_F(CppADCGPatternTestLoopDep, SimpleAtomic) {
     using namespace CppAD;
 
-
-    size_t m = 4;
-    size_t n = 4;
+    size_t m = 8;
+    size_t n = 8;
     size_t repeat = 6;
 
     // create atomic function
-    std::vector<AD<double> > y(4), x(n);
+    std::vector<AD<double> > y(4), x(4);
     checkpoint<double> atomicfun("atomicFunc2", atomicFunction, x, y);
     CGAtomicFun<double> cgatomicfun(atomicfun, true);
     PatternTestModelWithAtom<CGD> model(outerModel, cgatomicfun);
