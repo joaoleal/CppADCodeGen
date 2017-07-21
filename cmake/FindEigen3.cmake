745cb0a66d7045160cb05e984c83c7047fd85d06
Joao Leal <joaoruileal@gmail.com>
2017-07-21 13:43:31 +0100

    removed line used for debugging CMake

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -61,8 +61,6 @@ if (EIGEN3_INCLUDE_DIR)
 
 else (EIGEN3_INCLUDE_DIR)
 
-    message($ENV{EIGEN_HOME})
-
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       HINTS
       $ENV{EIGEN_HOME}
